b4ddd792f11e41ab5a55fd2e66b7cc3b00358fab
hashimshah47 <hashimtayyab47@gmail.com>
2024-08-25 18:39:19 +0500

    update index

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+app.get("/", (req, res) => res.send("Express on Vercel"));
 require('./routes/auth.route')(app)
 require('./routes/user.route')(app)
 require('./routes/configuration.route')(app)
